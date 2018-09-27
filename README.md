3ae499335b772134e9133b977a2ea5060c5abe5d
Bryant Fukushima <bmfukushima94@gmail.com>
2018-09-27 02:01:58 -0700

    scrape data added to db; display all data from db collection article

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mongo Scraper
+# Mongo Scraper: Surfer Scrape
 
 Mongo Scraper scrapes the Sufer's features page. (https://www.surfer.com/features/) and adds each features information into a MongoDB database. Each feature from the Surfer's website is then displayed onto the page. Users are able to add comments to each feature on the page.
 
--- server.js
+++ server.js
@@ -25,34 +25,63 @@ const databaseUrl = "surfscraper";
 const collections = ["article" , "comments"];
 
 // Use mongojs to hook the database to the db variable
-const db = mongojs(databaseUrl, collections);
+const db = mongojs(databaseUrl , collections);
 
 // This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
+db.on("error", (error) => {
   console.log("Database Error:", error);
 });
 
 app.get("/" , (req , res) => {
-  res.render("pages/");
+  db.article.find({} , (err , found) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+  // res.render("pages/");
 });
 
 app.get("/scrape" , (req , res) => {
-  request("https://www.surfer.com/features/", function(error, response, html) {
+  request("https://www.surfer.com/features/", (error , response , html) => {
     let results = [];
     const $ = cheerio.load(html);
 
     $(".entry-container").each((i , element) => {
-      console.log($(element).children(".entry-header").children().children().text());
+      // console.log($(element).children(".entry-header").children().children().text());
       let title = $(element).children(".entry-header").children().children().text();
 
-      results.push({
-        title: title
-      });
+      let excerpt = $(element).children(".selects-content").children().clone().children().remove().end().text();
+
+      let link = $(element).children(".selects-content").children().children().attr("href");
+
+
+      if(title && excerpt && link) {
+        db.article.createIndex({"title": title} , { unique:true });
+        db.article.insert({
+          title: title,
+          excerpt: excerpt + "...",
+          link: link
+        }, (err , inserted) => {
+          if(inserted) {
+            // TODO: global var to count amount of new articles
+            // NOTE: outside loop call if "var" is 0 then no new articles, else added "x" amount of articles
+            console.log("Scrape Successful")
+          } else if(err.code == 11000) {
+            console.log("No new articles");
+          } else {
+            console.log(err);
+          }
+        });
+      }
     });
+    // TODO: reroute scrape to homepage
     res.json(results);
   });
 });
 
-app.listen(3000, function() {
+
+app.listen(3000, () => {
     console.log("listening on 3000");
 });
